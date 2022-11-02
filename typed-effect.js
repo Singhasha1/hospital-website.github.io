f753f8d16674d44103f89397ac076e36fcc8de4a
Singhasha1 <112199624+Singhasha1@users.noreply.github.com>
2022-11-02 18:11:12 +0530

    Initial commit

---
+++ typed-effect.js
@@ -0,0 +1,9 @@
+//code for typing effect from typed.js cdn
+
+var typed = new typed(".type-effect",{
+    strings: ["Welcome To Life Care", "We Care Your Health", "We Are Expert!"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+})
+
